43c4b34bcd0b57e1a8a5fe1956bc8b72d18c4e4b
ib-kanri <k.hamada.ib@fisty.jp>
2026-01-28 16:39:54 +0900

    Add files via upload

---
+++ kubun_items.js
@@ -0,0 +1,870 @@
+const KUBUN_ITEMS = {
+  "docomo稼働": [
+    {
+      "section": "モバイル"
+    },
+    {
+      "key": "新規契約",
+      "label": "新規契約",
+      "type": "number"
+    },
+    {
+      "key": "MNP",
+      "label": "MNP",
+      "type": "number"
+    },
+    {
+      "key": "機種変更",
+      "label": "機種変更",
+      "type": "number"
+    },
+    {
+      "key": "マイグレ",
+      "label": "マイグレ",
+      "type": "number"
+    },
+    {
+      "section": "固定回線"
+    },
+    {
+      "key": "docomo光 1ギガ",
+      "label": "docomo光 1ギガ",
+      "type": "number"
+    },
+    {
+      "key": "docomo光 10ギガ",
+      "label": "docomo光 10ギガ",
+      "type": "number"
+    },
+    {
+      "key": "home5G",
+      "label": "home5G",
+      "type": "number"
+    },
+    {
+      "key": "homeでんわ",
+      "label": "homeでんわ",
+      "type": "number"
+    },
+    {
+      "section": "その他"
+    },
+    {
+      "key": "タブレット",
+      "label": "タブレット",
+      "type": "number"
+    },
+    {
+      "key": "でんき",
+      "label": "でんき",
+      "type": "number"
+    },
+    {
+      "key": "スカパー",
+      "label": "スカパー",
+      "type": "number"
+    },
+    {
+      "key": "H4D",
+      "label": "H4D",
+      "type": "number"
+    },
+    {
+      "key": "Disney+",
+      "label": "Disney+",
+      "type": "number"
+    },
+    {
+      "key": "dカード",
+      "label": "dカード",
+      "type": "number"
+    },
+    {
+      "key": "dカード（ゴールド）",
+      "label": "dカード（ゴールド）",
+      "type": "number"
+    },
+    {
+      "key": "dカード（プラチナ）",
+      "label": "dカード（プラチナ）",
+      "type": "number"
+    }
+  ],
+  "ショップ稼働｜au・UQ": [
+    {
+      "section": "au"
+    },
+    {
+      "key": "新規契約",
+      "label": "新規契約",
+      "type": "number"
+    },
+    {
+      "key": "MNP",
+      "label": "MNP",
+      "type": "number"
+    },
+    {
+      "key": "機種変更",
+      "label": "機種変更",
+      "type": "number"
+    },
+    {
+      "key": "アップグレード",
+      "label": "アップグレード",
+      "type": "number"
+    },
+    {
+      "section": "UQ mobile"
+    },
+    {
+      "key": "新規契約",
+      "label": "新規契約",
+      "type": "number"
+    },
+    {
+      "key": "MNP",
+      "label": "MNP",
+      "type": "number"
+    },
+    {
+      "key": "機種変更",
+      "label": "機種変更",
+      "type": "number"
+    },
+    {
+      "key": "ダウングレード",
+      "label": "ダウングレード",
+      "type": "number"
+    },
+    {
+      "section": "固定回線"
+    },
+    {
+      "key": "auひかり",
+      "label": "auひかり",
+      "type": "number"
+    },
+    {
+      "key": "BIGLOBE光",
+      "label": "BIGLOBE光",
+      "type": "number"
+    },
+    {
+      "key": "ケーブルトスアップ",
+      "label": "ケーブルトスアップ",
+      "type": "number"
+    },
+    {
+      "key": "UQ WiMAX",
+      "label": "UQ WiMAX",
+      "type": "number"
+    },
+    {
+      "key": "その他",
+      "label": "その他",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード（シルバー）",
+      "label": "au PAYカード（シルバー）",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード（ゴールド）新規",
+      "label": "au PAYカード（ゴールド）新規",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード（ゴールド）ランクアップ",
+      "label": "au PAYカード（ゴールド）ランクアップ",
+      "type": "number"
+    },
+    {
+      "key": "じぶん銀行",
+      "label": "じぶん銀行",
+      "type": "number"
+    },
+    {
+      "key": "でんき",
+      "label": "でんき",
+      "type": "number"
+    },
+    {
+      "key": "ガス",
+      "label": "ガス",
+      "type": "number"
+    },
+    {
+      "key": "おうち",
+      "label": "おうち",
+      "type": "number"
+    },
+    {
+      "key": "自転車",
+      "label": "自転車",
+      "type": "number"
+    },
+    {
+      "key": "わんにゃん",
+      "label": "わんにゃん",
+      "type": "number"
+    },
+    {
+      "key": "プラスワン",
+      "label": "プラスワン",
+      "type": "amount"
+    }
+  ],
+  "量販店稼働｜au・UQ": [
+    {
+      "section": "au"
+    },
+    {
+      "key": "新規契約（ワンクロ）",
+      "label": "新規契約（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "新規契約（キャッチのみ）",
+      "label": "新規契約（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "新規契約（登録のみ）",
+      "label": "新規契約（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（ワンクロ）",
+      "label": "MNP（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（キャッチのみ）",
+      "label": "MNP（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（登録のみ）",
+      "label": "MNP（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（ワンクロ）",
+      "label": "機種変更（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（キャッチのみ）",
+      "label": "機種変更（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（登録のみ）",
+      "label": "機種変更（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "アップグレード（ワンクロ）",
+      "label": "アップグレード（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "アップグレード（キャッチのみ）",
+      "label": "アップグレード（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "アップグレード（登録のみ）",
+      "label": "アップグレード（登録のみ）",
+      "type": "number"
+    },
+    {
+      "section": "UQ mobile"
+    },
+    {
+      "key": "新規契約（ワンクロ）",
+      "label": "新規契約（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "新規契約（キャッチのみ）",
+      "label": "新規契約（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "新規契約（登録のみ）",
+      "label": "新規契約（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（ワンクロ）",
+      "label": "MNP（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（キャッチのみ）",
+      "label": "MNP（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（登録のみ）",
+      "label": "MNP（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（ワンクロ）",
+      "label": "機種変更（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（キャッチのみ）",
+      "label": "機種変更（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（登録のみ）",
+      "label": "機種変更（登録のみ）",
+      "type": "number"
+    },
+    {
+      "section": "固定回線・その他"
+    },
+    {
+      "key": "auひかり（ワンクロ）",
+      "label": "auひかり（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "auひかり（キャッチのみ）",
+      "label": "auひかり（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "auひかり（登録のみ）",
+      "label": "auひかり（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "BIGLOBE光（ワンクロ）",
+      "label": "BIGLOBE光（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "BIGLOBE光（キャッチのみ）",
+      "label": "BIGLOBE光（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "BIGLOBE光（登録のみ）",
+      "label": "BIGLOBE光（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード（ワンクロ）",
+      "label": "au PAYカード（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード（キャッチのみ）",
+      "label": "au PAYカード（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード（登録のみ）",
+      "label": "au PAYカード（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード ゴールド（ワンクロ）",
+      "label": "au PAYカード ゴールド（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード ゴールド（キャッチのみ）",
+      "label": "au PAYカード ゴールド（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "au PAYカード ゴールド（登録のみ）",
+      "label": "au PAYカード ゴールド（登録のみ）",
+      "type": "number"
+    }
+  ],
+  "ショップ稼働｜SB・YM": [
+    {
+      "section": "SoftBank"
+    },
+    {
+      "key": "新規契約",
+      "label": "新規契約",
+      "type": "number"
+    },
+    {
+      "key": "MNP",
+      "label": "MNP",
+      "type": "number"
+    },
+    {
+      "key": "機種変更",
+      "label": "機種変更",
+      "type": "number"
+    },
+    {
+      "key": "アップグレード",
+      "label": "アップグレード",
+      "type": "number"
+    },
+    {
+      "section": "Y!mobile"
+    },
+    {
+      "key": "新規契約",
+      "label": "新規契約",
+      "type": "number"
+    },
+    {
+      "key": "MNP",
+      "label": "MNP",
+      "type": "number"
+    },
+    {
+      "key": "機種変更",
+      "label": "機種変更",
+      "type": "number"
+    },
+    {
+      "key": "ダウングレード",
+      "label": "ダウングレード",
+      "type": "number"
+    },
+    {
+      "section": "固定・その他"
+    },
+    {
+      "key": "SoftBank光",
+      "label": "SoftBank光",
+      "type": "number"
+    },
+    {
+      "key": "SoftBank Air",
+      "label": "SoftBank Air",
+      "type": "number"
+    },
+    {
+      "key": "おうちでんわ",
+      "label": "おうちでんわ",
+      "type": "number"
+    },
+    {
+      "key": "PayPayカード",
+      "label": "PayPayカード",
+      "type": "number"
+    },
+    {
+      "key": "PayPayカード ゴールド",
+      "label": "PayPayカード ゴールド",
+      "type": "number"
+    },
+    {
+      "key": "セレクション金額",
+      "label": "セレクション金額",
+      "type": "amount"
+    }
+  ],
+  "量販店稼働｜SB・YM": [
+    {
+      "section": "SoftBank"
+    },
+    {
+      "key": "新規契約（ワンクロ）",
+      "label": "新規契約（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "新規契約（キャッチのみ）",
+      "label": "新規契約（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "新規契約（登録のみ）",
+      "label": "新規契約（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（ワンクロ）",
+      "label": "MNP（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（キャッチのみ）",
+      "label": "MNP（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（登録のみ）",
+      "label": "MNP（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（ワンクロ）",
+      "label": "機種変更（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（キャッチのみ）",
+      "label": "機種変更（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（登録のみ）",
+      "label": "機種変更（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "アップグレード（ワンクロ）",
+      "label": "アップグレード（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "アップグレード（キャッチのみ）",
+      "label": "アップグレード（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "アップグレード（登録のみ）",
+      "label": "アップグレード（登録のみ）",
+      "type": "number"
+    },
+    {
+      "section": "Y!mobile"
+    },
+    {
+      "key": "新規契約（ワンクロ）",
+      "label": "新規契約（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "新規契約（キャッチのみ）",
+      "label": "新規契約（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "新規契約（登録のみ）",
+      "label": "新規契約（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（ワンクロ）",
+      "label": "MNP（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（キャッチのみ）",
+      "label": "MNP（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "MNP（登録のみ）",
+      "label": "MNP（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（ワンクロ）",
+      "label": "機種変更（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（キャッチのみ）",
+      "label": "機種変更（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "機種変更（登録のみ）",
+      "label": "機種変更（登録のみ）",
+      "type": "number"
+    },
+    {
+      "section": "固定回線・その他"
+    },
+    {
+      "key": "SoftBank光（ワンクロ）",
+      "label": "SoftBank光（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "SoftBank光（キャッチのみ）",
+      "label": "SoftBank光（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "SoftBank光（登録のみ）",
+      "label": "SoftBank光（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "SoftBank Air（ワンクロ）",
+      "label": "SoftBank Air（ワンクロ）",
+      "type": "number"
+    },
+    {
+      "key": "SoftBank Air（キャッチのみ）",
+      "label": "SoftBank Air（キャッチのみ）",
+      "type": "number"
+    },
+    {
+      "key": "SoftBank Air（登録のみ）",
+      "label": "SoftBank Air（登録のみ）",
+      "type": "number"
+    },
+    {
+      "key": "おうちのでんわ",
+      "label": "おうちのでんわ",
+      "type": "number"
+    },
+    {
+      "key": "PayPayカード",
+      "label": "PayPayカード",
+      "type": "number"
+    },
+    {
+      "key": "PayPayカード ゴールド",
+      "label": "PayPayカード ゴールド",
+      "type": "number"
+    }
+  ],
+  "J:COM稼働": [
+    {
+      "section": "SDU/アップセル"
+    },
+    {
+      "key": "シンスタンダードプラス",
+      "label": "シンスタンダードプラス",
+      "type": "number"
+    },
+    {
+      "key": "シンスタンダード",
+      "label": "シンスタンダード",
+      "type": "number"
+    },
+    {
+      "key": "スタンダード",
+      "label": "スタンダード",
+      "type": "number"
+    },
+    {
+      "key": "フレックス",
+      "label": "フレックス",
+      "type": "number"
+    },
+    {
+      "key": "セレクト",
+      "label": "セレクト",
+      "type": "number"
+    },
+    {
+      "key": "リンクミニ",
+      "label": "リンクミニ",
+      "type": "number"
+    },
+    {
+      "key": "光10G/FTTH",
+      "label": "光10G/FTTH",
+      "type": "number"
+    },
+    {
+      "key": "光10G/au",
+      "label": "光10G/au",
+      "type": "number"
+    },
+    {
+      "key": "光1G/au",
+      "label": "光1G/au",
+      "type": "number"
+    },
+    {
+      "key": "ドクシス1G",
+      "label": "ドクシス1G",
+      "type": "number"
+    },
+    {
+      "key": "固定電話",
+      "label": "固定電話",
+      "type": "number"
+    },
+    {
+      "section": "MDU/アップセル"
+    },
+    {
+      "key": "シンスタンダードプラス",
+      "label": "シンスタンダードプラス",
+      "type": "number"
+    },
+    {
+      "key": "シンスタンダード（通常）",
+      "label": "シンスタンダード（通常）",
+      "type": "number"
+    },
+    {
+      "key": "シンスタンダード（相対）",
+      "label": "シンスタンダード（相対）",
+      "type": "number"
+    },
+    {
+      "key": "シンスタンダード（バルク）",
+      "label": "シンスタンダード（バルク）",
+      "type": "number"
+    },
+    {
+      "key": "スタンダード",
+      "label": "スタンダード",
+      "type": "number"
+    },
+    {
+      "key": "フレックス",
+      "label": "フレックス",
+      "type": "number"
+    },
+    {
+      "key": "セレクト",
+      "label": "セレクト",
+      "type": "number"
+    },
+    {
+      "key": "リンクミニ",
+      "label": "リンクミニ",
+      "type": "number"
+    },
+    {
+      "key": "ドクシス1G（通常）",
+      "label": "ドクシス1G（通常）",
+      "type": "number"
+    },
+    {
+      "key": "ドクシス1G（相対）",
+      "label": "ドクシス1G（相対）",
+      "type": "number"
+    },
+    {
+      "key": "ドクシス1G（バルク）",
+      "label": "ドクシス1G（バルク）",
+      "type": "number"
+    },
+    {
+      "key": "ドクシス320M（通常）",
+      "label": "ドクシス320M（通常）",
+      "type": "number"
+    },
+    {
+      "key": "ドクシス320M（相対）",
+      "label": "ドクシス320M（相対）",
+      "type": "number"
+    },
+    {
+      "key": "ドクシス320M（バルク）",
+      "label": "ドクシス320M（バルク）",
+      "type": "number"
+    },
+    {
+      "key": "固定電話",
+      "label": "固定電話",
+      "type": "number"
+    }
+  ],
+  "物販稼働": [
+    {
+      "key": "アプローチ数",
+      "label": "アプローチ数",
+      "type": "number"
+    },
+    {
+      "key": "提案数",
+      "label": "提案数",
+      "type": "number"
+    },
+    {
+      "key": "販売台数（自社メーカー）",
+      "label": "販売台数（自社メーカー）",
+      "type": "number"
+    },
+    {
+      "key": "販売台数（他社メーカー）",
+      "label": "販売台数（他社メーカー）",
+      "type": "number"
+    }
+  ],
+  "FISTY稼働": [
+    {
+      "key": "新規（無料ユーザー）",
+      "label": "新規（無料ユーザー）",
+      "type": "number"
+    },
+    {
+      "key": "新規（有料ユーザー）",
+      "label": "新規（有料ユーザー）",
+      "type": "number"
+    },
+    {
+      "key": "PV数",
+      "label": "PV数",
+      "type": "number"
+    },
+    {
+      "key": "商談数",
+      "label": "商談数",
+      "type": "number"
+    },
+    {
+      "key": "コンテンツ制作",
+      "label": "コンテンツ制作",
+      "type": "number"
+    },
+    {
+      "key": "完了タスク・補足事項",
+      "label": "完了タスク・補足事項",
+      "type": "textarea"
+    }
+  ],
+  "本社稼働": [
+    {
+      "key": "業務内容",
+      "label": "業務内容",
+      "type": "textarea"
+    },
+    {
+      "key": "完了タスク・補足事項",
+      "label": "完了タスク・補足事項",
+      "type": "textarea"
+    }
+  ],
+  "ラウンダー稼働": [
+    {
+      "key": "巡回店舗数",
+      "label": "巡回店舗数",
+      "type": "number"
+    },
+    {
+      "key": "所感・実施内容",
+      "label": "所感・実施内容",
+      "type": "textarea"
+    }
+  ],
+  "インターン・イベント稼働": [
+    {
+      "key": "アプローチ数",
+      "label": "アプローチ数",
+      "type": "number"
+    },
+    {
+      "key": "着座数",
+      "label": "着座数",
+      "type": "number"
+    },
+    {
+      "key": "成約数（トス含む）",
+      "label": "成約数（トス含む）",
+      "type": "number"
+    }
+  ]
+};
